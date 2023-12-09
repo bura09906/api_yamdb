885530f5223f26202eea9a8cd417e7e94c570e62
Малышев Никита Геннадьевич <m.nikita.g@yandex.ru>
2023-12-09 17:21:26 +0300

    Add models into inpor_data command

--- api_yamdb/titles/management/commands/import_csv.py
+++ api_yamdb/titles/management/commands/import_csv.py
@@ -1,9 +1,10 @@
 import csv
 from typing import Any
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 from titles.models import Category, Genre, Title, GenreTitle
 
-
+User = get_user_model()
 
 class Command(BaseCommand):
     help = 'Imports data from a CSV file into all models. WARNING old data will be erased!'
@@ -52,4 +53,42 @@ class Command(BaseCommand):
                     id=row['id'],
                     title=Title.objects.get(id=row['title_id']),
                     genre=Genre.objects.get(id=row['genre_id']),
+                )
+
+        User.objects.all().delete()
+        with open('static/data/users.csv', encoding='utf8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                User.objects.create(
+                    id=row['id'],
+                    username=row['username'],
+                    email=row['email'],
+                    role=row['role'],
+                    bio=row['bio'],
+                    first_name=row['first_name'],
+                    last_name=row['last_name']
+                )
+
+        Review.objects.all().delete()
+        with open('static/data/review.csv', encoding='utf8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                Review.objects.create(
+                    id=row['id'],
+                    title=Title.objects.get(id=row['title_id']),
+                    text=row['text'],
+                    author=User.objects.get(id=row['author']),
+                    score=row['score'],
+                    pub_date=row['pub_date']
+                )
+
+        Comment.objects.all().delete()
+        with open('static/data/review.csv', encoding='utf8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                Comment.objects.create(
+                    id=row['id'],
+                    review=Review.objects.get(row['review_id']),
+                    text=row['text'],
+                    pub_date=row['pub_date']
                 )
