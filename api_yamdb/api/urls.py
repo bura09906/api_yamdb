5883a74dbb9a694f1979232fa5bc43369e1a2b15
Дмитрий Бурмистров <132254303+bura09906@users.noreply.github.com>
2023-12-10 20:24:20 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -7,26 +7,19 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import GetTokenApiView, RegistationApiView, UserViewSet
 
-router = DefaultRouter()
-router.register(r'comments', CommentViewSet, basename='comments')
-router.register(r'reviews', ReviewViewSet, basename='reviews')
-
-urlpatterns = [
-    path('/titles/<int:title_id>/', include(router.urls)),
-    path('/titles/<int:title_id>/reviews/', include(router.urls)),
-    path('/titles/<int:title_id>/reviews/<int:review_id>/',
-         include(router.urls)),
-    path('/titles/<int:title_id>/reviews/<int:review_id>/comments/',
-         include(router.urls)),
-
-from .views import GetTokenApiView, RegistationApiView, UserViewSet
-
-
 router_v1 = DefaultRouter()
 router_v1.register(r'users', UserViewSet, basename='user')
+router_v1.register(r'comments', CommentViewSet, basename='comments')
+router_v1.register(r'reviews', ReviewViewSet, basename='reviews')
 
 urlpatterns = [
-    path(f'auth/signup/', RegistationApiView.as_view()),
-    path(f'auth/token/', GetTokenApiView.as_view()),
-    path(f'', include(router_v1.urls))
+    path('auth/signup/', RegistationApiView.as_view()),
+    path('auth/token/', GetTokenApiView.as_view()),
+    path('', include(router_v1.urls)),
+    path('/titles/<int:title_id>/', include(router_v1.urls)),
+    path('/titles/<int:title_id>/reviews/', include(router_v1.urls)),
+    path('/titles/<int:title_id>/reviews/<int:review_id>/',
+         include(router_v1.urls)),
+    path('/titles/<int:title_id>/reviews/<int:review_id>/comments/',
+         include(router_v1.urls)),
 ]
