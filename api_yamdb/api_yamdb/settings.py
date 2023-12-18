776962da70515780b90e9f37f43d5a24e1d807f7
Дмитрий Бурмистров <132254303+bura09906@users.noreply.github.com>
2023-12-18 09:16:53 +0300

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -139,13 +139,6 @@ REST_FRAMEWORK = {
 }
 
 
-SIMPLE_JWT = {
-    # Устанавливаем срок жизни токена
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'AUTH_HEADER_TYPES': ('Bearer',),
-}
-
-
 # Настройки JWT
 
 SIMPLE_JWT = {
